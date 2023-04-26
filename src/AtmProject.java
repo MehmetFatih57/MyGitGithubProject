a4074c1cecf39e2badc3f36dadd891bc93c3041b
Mehmet Fatih YILDIRIM <mehmet.fatihh57@gmail.com>
2023-04-26 22:00:14 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,5 +2,7 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Mecnun ne yaptin ?");
         //version1
+        System.out.println("Nasiiiiiiiiiiiiil");
+        //version2
     }
 }
