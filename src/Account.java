26292635a76896b97c7accaf118706bb4e7a400b
Mehmet Fatih YILDIRIM <mehmet.fatihh57@gmail.com>
2023-04-27 15:53:43 +0300

    new

--- src/Account.java
+++ src/Account.java
@@ -2,6 +2,7 @@ public class Account {
     public static void main(String[] args) {
         System.out.println("Leyla gitti abi...");
         System.out.println("Leyla gitti abi... :(");
+        System.out.println("Ismail  abi...");
     }
     //mhmt_fth2
 }
